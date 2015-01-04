1e421008bfb5b88e47e740f11366802b05b99834
shaunagm <shaunagm@gmail.com>
2015-01-04 01:53:02 +0000

    Changed course type to OSCTC

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,197 +3,37 @@ class Course < ActiveHash::Base
   self.data = [
     {
       id: 1,
-      name: 'RAILS',
-      title: 'Ruby on Rails',
-      description: 'This is a Ruby on Rails event. The focus will be on developing functional web apps and programming in Ruby.  You can find all the curriculum materials at <a href="http://docs.railsbridge.org">docs.railsbridge.org</a>.',
+      name: 'OSCTC-full',
+      title: 'Open Source Comes to Campus',
+      description: 'Our full-day introductory workshop. Typically includes tutorials on topics like open source community norms, issue trackers, IRC and GIt; a career panel; and a contributions workshop where attendees make their first contributions to FOSS projects.',
       levels: [
         {
           level: 1,
           color: 'blue',
-          title: "Totally New to Programming",
+          title: "Totally New to Open Source",
           level_description: [
-            'You have little to no experience with the terminal or a graphical IDE',
-            'You might have done a little bit with HTML or CSS, but not necessarily',
-            'You\'re unfamiliar with terms like methods, arrays, lists, hashes, or dictionaries.'
+            'You\'ve heard of open source or free software, but don\'t know much about them.',
+            'You\'ve never used IRC, issue trackers or Github.',
+	    'You haven\'t used a command line, or you have, but you\'re not comfortable with it.',
           ]
         }, {
           level: 2,
           color: 'green',
-          title: "Somewhat New to Programming",
+          title: "Somewhat New to Open Source",
           level_description: [
-            'You may have used the terminal a little — to change directories, for instance',
-            'You might have done an online programming tutorial or two',
-            'You don\'t have a lot of experience with Rails',
-            'You know what a method is',
-            'You aren\'t totally clear on how a request gets from the browser to your app'
+	    'You\'re comfortable using the command line/terminal.',
+            'You know what open source is, and have used open source projects and tools like Firefox, Wordpress, or Linux.',
+            'You know what version control, IRC, and/or issue trackers are but aren\'t really comfortable with them.',
           ]
         }, {
           level: 3,
           color: 'gold',
-          title: "Some Rails Experience",
+          title: "Some Open Source Experience",
           level_description: [
-            'You\'re comfortable using the terminal, but not necessarily a Power User',
-            'You have a general understanding of a Rails app\'s structure, perhaps from a prior workshop or tutorial',
-            'You know how to define a method in Ruby',
-            'You have a decent handle on Ruby arrays and hashes',
-          ]
-        }, {
-          level: 4,
-          color: 'orange',
-          title: "Other Programming Experience",
-          level_description: [
-            'You\'re proficient in another language and understand general programming concepts, like collections and scope.',
-            'You\'re new to Ruby and Rails',
-            'You might be familiar with version control and basic web architecture'
-          ]
-        }, {
-          level: 5,
-          color: 'purple',
-          title: "Ready for the Next Challenge",
-          level_description: [
-            'You\'ve exhausted the fun of the Suggestotron/Intro Rails curriculum',
-            'You\'re comfortable with the terminal',
-            'You want to problem-solve instead of copying other\'s code',
-            'You want to build an app without using scaffolds'
+	    'You\'re familiar with open source and have contributed or tried to contribute before.',
+	    'You know your way around issue trackers and platforms like Github.',
           ]
         }
-      ]
-    }, {
-      id: 2,
-      name: 'FRONTEND',
-      title: 'Front End',
-      description: 'This is a Front End workshop. The focus will be on designing web apps with HTML and CSS.  You can find all the curriculum materials at <a href="http://docs.railsbridge.org/frontend">docs.railsbridge.org/frontend</a>.',
-      levels: [
-        {
-          level: 1,
-          color: 'blue',
-          title: "Totally new to HTML and CSS",
-          level_description: [
-            'Perhaps has seen it before, but not written much (if any)',
-            'Not sure what tags, attributes, or selectors are',
-            '&lt;img&gt;, &lt;a&gt;, and &lt;p&gt; are exciting and new',
-          ]
-        }, {
-          level: 2,
-          color: 'green',
-          title: "Some experience with HTML",
-          level_description: [
-            'New to CSS',
-            'Perhaps has worked with a WYSIWIG editor but hasn\'t coded an HTML document from scratch',
-            'Has heard of a tags or attributes before, but isn\'t sure what they are',
-            'Recognizes <i>&lt;a href="http://google.com"&gt;this&lt;/a&gt;</i> but couldn\'t define what each piece means'
-          ]
-        }, {
-          level: 3,
-          color: 'gold',
-          title: "Some experience with HTML & CSS",
-          level_description: [
-            'Has possibly worked with the web inspector in Chrome or Firebug in Firefox before',
-            'Could possibly write a link in HTML',
-            'Not totally comfortable with CSS, but gets the basics'
-          ]
-        }, {
-          level: 4,
-          color: 'orange',
-          title: "Comfortable editing CSS & HTML",
-          level_description: [
-            'Knows about web development, but not a lot of front end experience',
-            'Maybe knows a programming language',
-            'Perhaps has used the Web Inspector before'
-          ]
-        }, {
-          level: 5,
-          color: 'purple',
-          title: "Ready to make a beautiful site",
-          level_description: [
-            'Knows how to include a stylesheet in an HTML document',
-            'Feels comfortable with terminology like tag and attribute',
-            'Has made and deployed a website with custom CSS or used a framework like Bootstrap or Foundation'
-          ]
-        }
-      ]
-    }, {
-      id: 3,
-      name: 'JAVASCRIPT',
-      title: 'Intro to Javascript',
-      description: 'This workshop will teach programming using Javascript.'\
-                   'You can find all the curriculum materials at <a'\
-                   'href="http://snake-tutorial.zeespencer.com.s3-website-us'\
-                   '-west-2.amazonaws.com/lesson-1/">the temporary location'\
-                   '</a>.',
-      levels: [
-        {
-          level: 1,
-          color: 'blue',
-          title: "No Programming Experience",
-          level_description: [
-            'Totally new to Javascript itself',
-            'Made a webpage before, maybe at a RailsBridge Front End Workshop',
-            'No experience with programming languages other than HTML and CSS',
-          ]
-        }, {
-          level: 2,
-          color: 'orange',
-          title: "Programmer new to Javascript",
-          level_description: [
-            'Comfortable making a complex webpage',
-            'Some experience in a programming lanugage like ActionScript, C, Java, Ruby or Python',
-            'Has seen javascript, but didn\'t really understand how it worked',
-          ]
-        }
-      ]
-    }, {
-      id: 4,
-      name: 'iOS',
-      title: 'Intro to iOS Development',
-      description: 'This workshop will cover how to make an iOS application.',
-      levels: [
-        {
-          level: 1,
-          color: 'blue',
-          title: "Totally New to Programming",
-          level_description: [
-            'You have little to no experience with the terminal or a graphical IDE',
-            'You might have done a little bit with HTML or CSS, but not necessarily',
-          ]
-        }, {
-          level: 2,
-          color: 'green',
-          title: "Somewhat New to Programming",
-          level_description: [
-            'You may have used the terminal a little — to change directories, for instance',
-            'You might have done an online programming tutorial or two',
-            'You know what a method is',
-          ]
-        }, {
-          level: 3,
-          color: 'gold',
-          title: "Some iOS App Development Experience",
-          level_description: [
-            "You're comfortable using the terminal, but not necessarily a Power User",
-            'You have a general understanding of XCode and of iOS app structure, perhaps from a prior workshop or tutorial',
-            'Some programming experience in another language like Ruby, Python, C, Java',
-          ]
-        }, {
-          level: 4,
-          color: 'pink',
-          title: "Experienced Designer new to iOS programming",
-          level_description: [
-            "You're proficient in design tools (Adobe Creative Suite, etc)",
-            "You're familiar with general UI design principles",
-            "You might be familiar with HTML and CSS.",
-            "You're new to the XCode environment and to iOS app development",            
-          ]
-        }, {
-          level: 5,
-          color: 'orange',
-          title: "Experienced Programmer new to iOS programming",
-          level_description: [
-            "You're proficient in another language and understand general programming concepts",
-            "You're new to the XCode environment and to iOS app development",
-            'You might be familiar with version control',
-          ]
-        },        
       ]
     }
   ]
