80aa42f0a18b481e969fb6e46e7940c1992f0039
shaunagm <shaunagm@gmail.com>
2015-02-08 20:38:55 +0000

    Add pre-sprint session type to courses

--- app/models/course.rb
+++ app/models/course.rb
@@ -35,7 +35,42 @@ class Course < ActiveHash::Base
           ]
         }
       ]
-    }
+    },
+    {
+      id: 2,
+      name: 'OSCTCsprint',
+      title: 'Introduction to Open Source Contributing for Sprints, Hackathons, and other Events',
+      description: 'This short introductory workshop goes over the basics of open source contributing, including commonly used tools, community norms and jargon, how to pick tasks to work on, and how to work through common obstacles.',
+      levels: [
+        {
+          level: 1,
+          color: 'blue',
+          title: "Totally New to Open Source",
+          level_description: [
+            'You\'ve heard of open source or free software, but don\'t know much about them.',
+            'You\'ve never used IRC, issue trackers or Github.',
+	    'You haven\'t used a command line, or you have, but you\'re not comfortable with it.',
+          ]
+        }, {
+          level: 2,
+          color: 'green',
+          title: "Somewhat New to Open Source",
+          level_description: [
+	    'You\'re comfortable using the command line/terminal.',
+            'You know what open source is, and have used open source projects and tools like Firefox, Wordpress, or Linux.',
+            'You know what version control, IRC, and/or issue trackers are but aren\'t really comfortable with them.',
+          ]
+        }, {
+          level: 3,
+          color: 'gold',
+          title: "Some Open Source Experience",
+          level_description: [
+	    'You\'re familiar with open source and have contributed or tried to contribute before.',
+	    'You know your way around issue trackers and platforms like Github.',
+          ]
+        }
+      ]
+    },	
   ]
   enum_accessor :name
 end
